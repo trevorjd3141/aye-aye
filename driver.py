12b1cfcab7a4e921058b76c0c7e97a54083bca10
Trevor Dalton <trevorjd3141@gmail.com>
2021-03-09 20:40:29 -0700

    Automatically find most common seed words in dataset

--- driver.py
+++ driver.py
@@ -4,27 +4,32 @@ import grader
 import preprocessing
 import recall
 import basilisk
+import seedSelector
 
 OUTPUT_PATH = 'output\\'
 PROCESSED_DATA_NAME = 'processed_data.csv'
 DATA_PATH = 'data\\'
 CATEGORY = 'noun.food'
 OPTIONS = {
-    'preprocessing': True,
-    'recall': True,
-    'extraction': False,
-    'grading': False
+    'preprocessing': False,
+    'seed-selection': False,
+    'recall': False,
+    'extraction': True,
+    'grading': True
 }
 
 def main():
     if OPTIONS['preprocessing']:
         preprocessing.compile(DATA_PATH, PROCESSED_DATA_NAME)
 
+    if OPTIONS['seed-selection']:
+        seedSelector.compileCommonWords(join(DATA_PATH, PROCESSED_DATA_NAME), CATEGORY)
+
     if OPTIONS['recall']:
         recall.compileCategoryWords(join(DATA_PATH, PROCESSED_DATA_NAME), CATEGORY)
 
     if OPTIONS['extraction']:
-        basilisk.basilisk(CATEGORY, OUTPUT_PATH, True)
+        basilisk.basilisk(CATEGORY, OUTPUT_PATH, join(DATA_PATH, PROCESSED_DATA_NAME), True)
 
     if OPTIONS['grading']:
         grader.read(OUTPUT_PATH, CATEGORY)
