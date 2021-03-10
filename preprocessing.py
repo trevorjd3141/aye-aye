e2be82f845bd42b7ecdb57b7c6164e52c01b682b
Trevor Dalton <trevorjd3141@gmail.com>
2021-03-10 02:31:30 -0700

    Remove all HTML from data source in the preprocessing step

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,22 @@
 from os.path import join
 import pandas as pd
+import re
 
 CSV_NAMES = ['RAW_recipes.csv', 'RAW_interactions.csv']
 CSV_TEXT_COLUMNS = ['description', 'review']
+
+def clean(text):
+    text = re.sub('(?:<[^\/]+\/>|&[^;]+;)', '', text)
+    return text
 
 def compile(path, name):
     all_text = []
     for i, csv in enumerate(CSV_NAMES):
         text_column = CSV_TEXT_COLUMNS[i]
         df = pd.read_csv(join(path, csv))
+        df.dropna(inplace=True)
         for text in df[text_column]:
+            text = clean(text)
             all_text.append(text)
     all_text_df = pd.DataFrame({'text': all_text})
-    all_text_df.dropna(inplace=True)
     all_text_df.to_csv(join(path, name), index=False)
