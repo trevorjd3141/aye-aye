86a861669b66a8fd1f665b9583deba0b477c352e
Trevor Dalton <trevorjd3141@gmail.com>
2021-03-12 01:09:03 -0700

    generate line graph to show precision over time

--- grader.py
+++ grader.py
@@ -1,7 +1,22 @@
 import util
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
-def calculateFScore(recall, precision):
+IMPORTANT_MARKERS = (20, 40, 60, 80, 100)
+
+def draw_graph(points, path):
+    x = [point[0] for point in points]
+    y = [point[1] for point in points]
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set(xlabel='Total Words', ylabel='Precision', title='Precision for Top-K Words')
+    ax.grid()
+
+    fig.savefig(path)
+
+def calculate_f_score(recall, precision):
     if (recall + precision) == 0:
         return 0.00
     score = (2 * recall * precision)/(precision + recall)
@@ -9,7 +24,7 @@ def calculateFScore(recall, precision):
 
 # Reads in a results file and tests its accuracy
 def read(path, category):
-    words = util.fetchLines(f'{path}{category}.txt')
+    words = util.fetch_lines(f'{path}{category}.txt')
 
     data = []
     total = len(words)
@@ -18,8 +33,8 @@ def read(path, category):
         data.append([word, util.categorize(word), category, util.categorize(word) == category])
         if util.categorize(word) == category:
             correct += 1
-    precision = correct/total
-    accuracy = 100 * precision
+    overall_precision = correct/total
+    accuracy = 100 * overall_precision
     df = pd.DataFrame(data, columns=['Word', 'Correct Category', 'Guessed Category', 'Correct Guess'])
     df.to_csv(f'{path}{category}-results.csv', index=False)
 
@@ -28,11 +43,20 @@ def read(path, category):
     print(f'Labeled matches sent to {category}-results.csv')
     print()
 
-    allCategoryWords = util.fetchLines(f'recall\\{category}.txt')
-    totalCategoryWords = len(allCategoryWords)
-    recall = correct/totalCategoryWords
-    fScore = calculateFScore(recall, precision)
+    points = []
+    for marker in IMPORTANT_MARKERS:
+        if marker < len(data):
+            precision = round(len([result for result in data[:marker] if result[-1]])/total,2)
+            points.append((marker, precision))
+            print(f'Precision for the top {marker} extracted words is {precision}')
+    draw_graph(points, f'{path}{category}-precision.png')
+    print()
 
-    print(f'Final Precision: {round(precision, 3)}')
-    print(f'Final Recall: {round(recall, 3)}')
-    print(f'Final F-Score: {round(fScore, 3)}')
+    all_category_words = util.fetch_lines(f'recall\\{category}.txt')
+    total_category_words = len(all_category_words)
+    recall = correct/total_category_words
+    f_score = calculate_f_score(recall, overall_precision)
+
+    print(f'Final Overall Precision: {round(overall_precision, 3)}')
+    print(f'Final Overall Recall: {round(recall, 3)}')
+    print(f'Final Overall F-Score: {round(f_score, 3)}')
